3b07987f75380bd85d3fa54d09b645a4ee32f930
imtiaz-imti <151422815+imtiaz-imti@users.noreply.github.com>
2024-09-05 18:16:13 +0600

    6th commit

--- frontend/src/component/layout/Home.js
+++ frontend/src/component/layout/Home.js
@@ -1,10 +1,10 @@
 import React from 'react'
 import MetaData from './MetaData'
-const Home = async () => {
+const Home = () => {
   return (
     <div className='homecompo'>
        <MetaData title='Home Page'/>
-       <div className='text1'>WELCOME TO OUR APP</div>
+       <div className='text1'>WELCOME TO OUR WEBSITE</div>
        <div className='text2'>You will get the best product from here</div>
     </div>
   )
