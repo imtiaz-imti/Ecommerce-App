0c85c150029c4e61947048e49b133015c77a200c
imtiaz-imti <151422815+imtiaz-imti@users.noreply.github.com>
2024-09-05 18:36:51 +0600

    5th commit

--- frontend/src/component/layout/Home.js
+++ frontend/src/component/layout/Home.js
@@ -4,7 +4,7 @@ const Home = async () => {
   return (
     <div className='homecompo'>
        <MetaData title='Home Page'/>
-       <div className='text1'>WELCOME TO OUR SITE</div>
+       <div className='text1'>WELCOME TO OUR APP</div>
        <div className='text2'>You will get the best product from here</div>
     </div>
   )
