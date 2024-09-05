315675082b031f4faf3b44c31ab22280d2b6d3b1
imtiaz-imti <151422815+imtiaz-imti@users.noreply.github.com>
2024-09-05 18:31:11 +0600

    7th commit

--- frontend/src/component/layout/productAction.js
+++ frontend/src/component/layout/productAction.js
@@ -3,11 +3,12 @@ import { ALL_PRODUCT_FAIL,ALL_PRODUCT_SUCCESS,ALL_PRODUCT_REQUEST,ALL_PRODUCT_DE
 export const getProduct = (searchValue)=> async (dispatch)=>{
     try {
         dispatch({type:ALL_PRODUCT_REQUEST})
-        const {data} = await axios.get('/api/v1/products?keyword=' + searchValue)
+        const {data} = await axios.get('/api/v1/products')
         setTimeout(() => {
             dispatch({type:ALL_PRODUCT_SUCCESS,payload:data})
         },3000)
     } catch (error) {
+        console.log(error)
         dispatch({type:ALL_PRODUCT_FAIL,payload:error.response.data.message})
     }
 }
