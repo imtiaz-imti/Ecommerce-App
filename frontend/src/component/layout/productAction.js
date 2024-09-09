4f7ab2e7272eba1131d09cebfcf7e81b0146addc
imtiaz-imti <151422815+imtiaz-imti@users.noreply.github.com>
2024-09-09 13:38:47 +0600

    Update productAction.js

--- frontend/src/component/layout/productAction.js
+++ frontend/src/component/layout/productAction.js
@@ -1,11 +1,10 @@
 import axios from "axios"
 import { ALL_PRODUCT_FAIL,ALL_PRODUCT_SUCCESS,ALL_PRODUCT_REQUEST,ALL_PRODUCT_DETAILS_FAIL,ALL_PRODUCT_DETAILS_SUCCESS,ALL_PRODUCT_DETAILS_REQUEST} from './const'
+const api = axios.create({baseURL :'https://retail-market-app-backend.onrender.com'})
 export const getProduct = (searchValue)=> async (dispatch)=>{
     try {
         dispatch({type:ALL_PRODUCT_REQUEST})
-        const api = axios.create({baseURL : 'https://retail-market-app-backend.onrender.com'})
         const {data} = await api.get('/api/v1/products')
-        console.log(data)
         dispatch({type:ALL_PRODUCT_SUCCESS,payload:data})
     } catch (error) {
         console.log(error)
@@ -14,7 +13,7 @@ export const getProduct = (searchValue)=> async (dispatch)=>{
 }
 export const getProductNames = ()=> async (dispatch)=>{
     try {
-        const {data} = await axios.get('/api/v1/products')
+        const {data} = await api.get('/api/v1/products')
         dispatch({type:'PRODUCT_NAMES',payload:data.map(data=>data.name)})
     } catch (error) {
         console.log(error.message)
@@ -23,7 +22,7 @@ export const getProductNames = ()=> async (dispatch)=>{
 export const getProductDetails = (productID)=> async (dispatch)=>{
     try {
         dispatch({type:ALL_PRODUCT_DETAILS_REQUEST})
-        const {data} = await axios.get('/api/v1/admin/products/'+productID)
+        const {data} = await api.get('/api/v1/admin/products/'+productID)
         setTimeout(() => {
             dispatch({type:ALL_PRODUCT_DETAILS_SUCCESS,payload:data})
         },3000)
@@ -34,7 +33,7 @@ export const getProductDetails = (productID)=> async (dispatch)=>{
 export const getUserDetails = (userID)=> async (dispatch)=>{
     try {
         dispatch({type:ALL_PRODUCT_DETAILS_REQUEST})
-        const {data} = await axios.get('/api/v1/user/details')
+        const {data} = await api.get('/api/v1/user/details')
         dispatch({type:'set_user',payload:data.newUser})
     } catch (error) {
         dispatch({type:'user_fail',payload:null})
@@ -49,7 +48,7 @@ export const addOrder = (orderList)=> (dispatch)=>{
 }
 export const orderDet = ()=> async (dispatch)=>{
     try {
-        const orderDetails = await axios.get('/api/v1/order/details')
+        const orderDetails = await api.get('/api/v1/order/details')
         dispatch({type:'order_details',payload:orderDetails.data.newOrder})
     } catch (error) {
         dispatch({type:'order_details',payload:[]})
@@ -57,7 +56,7 @@ export const orderDet = ()=> async (dispatch)=>{
 }
 export const adminOrderDet = ()=> async (dispatch)=>{
     try {
-        const orderDetails = await axios.get('/api/v1/order/admin/details/all')
+        const orderDetails = await api.get('/api/v1/order/admin/details/all')
         dispatch({type:'admin_order_details',payload:orderDetails.data.newOrder})
     } catch (error) {
         dispatch({type:'order_details',payload:[]})
@@ -65,7 +64,7 @@ export const adminOrderDet = ()=> async (dispatch)=>{
 }
 export const getAllUser = ()=> async (dispatch)=>{
     try {
-        const allUser = await axios.get('/api/v1/user/admin/all')
+        const allUser = await api.get('/api/v1/user/admin/all')
         dispatch({type:'admin_all_users',payload:allUser.data.users})
     } catch (error) {
         dispatch({type:'admin_all_users_fail',payload:[]})
