ce5cb56aef7531f8f1984e976187adae131a7a02
imtiaz-imti <151422815+imtiaz-imti@users.noreply.github.com>
2024-09-12 20:12:20 +0600

    Update Signin.js

--- frontend/src/component/layout/Signin.js
+++ frontend/src/component/layout/Signin.js
@@ -22,7 +22,7 @@ const Signin = () => {
       axios.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded'
       const api = axios.create({baseURL :'https://retail-market-app-backend.onrender.com'})
       console.log(body)
-      await api.post('/api/v1/user/login',{"email":document.getElementById('email').value,"password":document.getElementById('password').value})
+      await api.post('/api/v1/user/login',body)
       dispatch(getUserDetails()) 
     } catch (error) {
       document.getElementById('emailwrong').style.visibility = 'visible'
