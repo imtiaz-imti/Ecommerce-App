f6ad5ccda9fd2835c7b478136d4706581bc08ad4
imtiaz-imti <151422815+imtiaz-imti@users.noreply.github.com>
2024-09-12 11:04:13 +0600

    Update extra.js

--- backend/extra.js
+++ backend/extra.js
@@ -40,7 +40,8 @@ const sendToken = (userNew,statusCode,res)=>{
      expires: new Date(Date.now() + process.env.COOKIE_EXPIRES * 24 * 60 * 60 * 1000),
      httpOnly:true
   }
-  return res.status(statusCode).setHeader('Access-Control-Allow-Origin', 'https://ecommerce-sb7c.onrender.com').cookie('token',token,options).json({success:true,message:'user logged in successfully',token,userNew:userNew._id})
+  // .cookie('token',token,options)
+  return res.status(statusCode).setHeader('Access-Control-Allow-Origin', 'https://ecommerce-sb7c.onrender.com').json({success:true,message:'user logged in successfully',token,userNew:userNew._id})
 }
 const isAuthenticatedUser = async (req,res,next)=>{
   try{
