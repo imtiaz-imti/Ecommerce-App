1142b04490f0a4723420a18e6f7fca0848034850
imtiaz-imti <151422815+imtiaz-imti@users.noreply.github.com>
2024-09-12 18:24:37 +0600

    Update Signin.js

--- frontend/src/component/layout/Signin.js
+++ frontend/src/component/layout/Signin.js
@@ -19,9 +19,7 @@ const Signin = () => {
         password:document.getElementById('password').value,
       }
       // 'application/x-www-form-urlencoded' text/plain
-      // axios.defaults.headers.post['Content-Type'] = 'application/json'
-      axios.defaults.headers.post['Content-Type'] ='application/json;charset=utf-8'
-      axios.defaults.headers.post['Access-Control-Allow-Origin'] = '*'
+      axios.defaults.headers.post['Content-Type'] = 'text/plain'
       const api = axios.create({baseURL :'https://retail-market-app-backend.onrender.com'})
       console.log(body)
       await api.post('/api/v1/user/login',body)
