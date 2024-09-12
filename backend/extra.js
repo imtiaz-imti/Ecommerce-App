45053948d876bf82193b7978fc496a9e1a95e5f8
imtiaz-imti <151422815+imtiaz-imti@users.noreply.github.com>
2024-09-12 12:45:26 +0600

    Update extra.js

--- backend/extra.js
+++ backend/extra.js
@@ -35,13 +35,12 @@ const objMatching = (obj1,obj2)=>{
   return match
 }
 const sendToken = (userNew,statusCode,res)=>{
-  // const token = userNew.getJWTToken()
-  // const options = {
-  //    expires: new Date(Date.now() + process.env.COOKIE_EXPIRES * 24 * 60 * 60 * 1000),
-  //    httpOnly:true
-  // }
-  // .cookie('token',token,options) ,token,userNew:userNew._id
-  return res.status(statusCode).setHeader('Access-Control-Allow-Origin', 'https://ecommerce-sb7c.onrender.com').json({success:true,message:'user logged in successfully'})
+  const token = userNew.getJWTToken()
+  const options = {
+     expires: new Date(Date.now() + process.env.COOKIE_EXPIRES * 24 * 60 * 60 * 1000),
+     httpOnly:true
+  }
+  return res.status(statusCode).setHeader('Access-Control-Allow-Origin', 'https://ecommerce-sb7c.onrender.com').cookie('token',token,options).json({success:true,message:'user logged in successfully',token,userNew:userNew._id})
 }
 const isAuthenticatedUser = async (req,res,next)=>{
   try{
