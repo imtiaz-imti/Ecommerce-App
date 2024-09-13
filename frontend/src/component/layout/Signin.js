01fb9a06669d6904f05eaa5ad97c39c8b21a8403
imtiaz-imti <151422815+imtiaz-imti@users.noreply.github.com>
2024-09-13 18:06:45 +0600

    Update Signin.js

--- frontend/src/component/layout/Signin.js
+++ frontend/src/component/layout/Signin.js
@@ -19,11 +19,21 @@ const Signin = () => {
         "email":document.getElementById('email').value,
         "password":document.getElementById('password').value,
       }
-      axios.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded; charset=UTF-8'
-      const api = axios.create({baseURL :'https://retail-market-app-backend.onrender.com'})
-      console.log(body)
-      await api.post('/api/v1/user/login',body)
-      dispatch(getUserDetails()) 
+      axios.post('https://retail-market-app-backend.onrender.com/api/v1/user/login', {
+    firstName: 'Fred',
+    lastName: 'Flintstone'
+  })
+  .then(function (response) {
+    console.log(response);
+  })
+  .catch(function (error) {
+    console.log(error);
+  });
+      // axios.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded; charset=UTF-8'
+      // const api = axios.create({baseURL :'https://retail-market-app-backend.onrender.com'})
+      // console.log(body)
+      // await api.post('/api/v1/user/login',body)
+      // dispatch(getUserDetails()) 
     } catch (error) {
       document.getElementById('emailwrong').style.visibility = 'visible'
       document.getElementById('passwordwrong').style.visibility = 'visible'
