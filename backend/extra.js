93872d91a2c3e3c93f48634d20e33feffd646281
imtiaz-imti <151422815+imtiaz-imti@users.noreply.github.com>
2024-09-13 21:38:17 +0600

    Update extra.js

--- backend/extra.js
+++ backend/extra.js
@@ -44,7 +44,6 @@ const sendToken = (userNew,statusCode,res)=>{
 }
 const isAuthenticatedUser = async (req,res,next)=>{
   try{
-     return res.status(401).setHeader('Access-Control-Allow-Origin', 'https://ecommerce-sb7c.onrender.com').json({success:true,token:req.cookies})
      const {token} = req.cookies
      if(!token){return next(new ErrorHander('please login to excess this resource',401))}
      const data =  jwt.verify(token,process.env.JWT_SECRET)
