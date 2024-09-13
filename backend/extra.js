2b58d435317922e0275153a9e1790d651463e311
imtiaz-imti <151422815+imtiaz-imti@users.noreply.github.com>
2024-09-13 19:27:22 +0600

    Update extra.js

--- backend/extra.js
+++ backend/extra.js
@@ -35,8 +35,8 @@ const objMatching = (obj1,obj2)=>{
   return match
 }
 const sendToken = (userNew,statusCode,res)=>{
-  const token = userNew.getJWTToken() || 'hello'
-  return res.status(200).setHeader('Access-Control-Allow-Origin', 'https://ecommerce-sb7c.onrender.com').json({success:true,message:'user logged in successfully',token,userNew:userNew._id})
+  const token = userNew.getJWTToken()
+  return res.status(200).setHeader('Access-Control-Allow-Origin', 'https://ecommerce-sb7c.onrender.com').json({success:true,message:'user logged in successfully',userNew:userNew._id,token})
   const options = {
      expires: new Date(Date.now() + process.env.COOKIE_EXPIRES * 24 * 60 * 60 * 1000),
      httpOnly:true
