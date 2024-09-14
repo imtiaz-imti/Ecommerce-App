8aaa889cac80566ed3310f210f5548b8a3efaa86
imtiaz-imti <151422815+imtiaz-imti@users.noreply.github.com>
2024-09-14 12:35:02 +0600

    Update extra.js

--- backend/extra.js
+++ backend/extra.js
@@ -46,7 +46,7 @@ const sendToken = (userNew,statusCode,res)=>{
 const isAuthenticatedUser = async (req,res,next)=>{
   try{
      console.log(req.cookies,'notun')
-     const token = req.cookies['token']
+     const {token} = req.cookies
      if(!token){return next(new ErrorHander('please login to excess this resource',401))}
      const data =  jwt.verify(token,process.env.JWT_SECRET)
      req.user = await user.findById(data.id)
