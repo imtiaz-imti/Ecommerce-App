587e1707d84b5d17cb19b956471e1b14f4419d7a
imtiaz-imti <151422815+imtiaz-imti@users.noreply.github.com>
2024-09-14 20:51:20 +0600

    Update productAction.js

--- frontend/src/component/layout/productAction.js
+++ frontend/src/component/layout/productAction.js
@@ -4,7 +4,7 @@ const api = axios.create({baseURL :'https://retail-market-app-backend.onrender.c
 export const getProduct = (searchValue)=> async (dispatch)=>{
     try {
         dispatch({type:ALL_PRODUCT_REQUEST})
-        const {data} = await api.get('/api/v1/products?'+searchValue)
+        const {data} = await api.get('/api/v1/products')
         dispatch({type:ALL_PRODUCT_SUCCESS,payload:data})
     } catch (error) {
         console.log(error)
