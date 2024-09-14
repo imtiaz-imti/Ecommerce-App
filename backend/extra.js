e9cd62e1e2ca3a6e799a143c0b4de97e06f47b32
imtiaz-imti <151422815+imtiaz-imti@users.noreply.github.com>
2024-09-14 13:12:28 +0600

    Update extra.js

--- backend/extra.js
+++ backend/extra.js
@@ -38,11 +38,11 @@ const sendToken = (userNew,statusCode,res)=>{
   const token = userNew.getJWTToken()
   const options = {
      httpOnly: true,
-     // secure: true,
-     // sameSite: 'none',
-     // domain: 'retail-market-app-backend.onrender.com',
-     // expires: token.expiresIn
-     expires: new Date(Date.now() + process.env.COOKIE_EXPIRES * 24 * 60 * 60 * 1000)
+     secure: true,
+     sameSite: 'none',
+     domain: 'retail-market-app-backend.onrender.com',
+     expires: token.expiresIn
+     // expires: new Date(Date.now() + process.env.COOKIE_EXPIRES * 24 * 60 * 60 * 1000)
   }
   // setHeader('Access-Control-Allow-Origin', 'https://ecommerce-sb7c.onrender.com')
   return res.status(statusCode).cookie('token',token,options).json({success:true,message:'user logged in successfully',token,userNew:userNew._id})
