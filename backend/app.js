0c2f74cbb3ad5b66a098a49f10f496bab1a8d674
imtiaz-imti <151422815+imtiaz-imti@users.noreply.github.com>
2024-09-14 11:50:50 +0600

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -15,9 +15,9 @@ app.use(cors({
 }))
 app.use(express.json())
 app.use('/api/v1',product)
+app.use(cookieParser())
 app.use('/api/v1/user',user)
 app.use('/api/v1/order',router)
 app.use(errorHandler)
 app.use(sendToken)
-app.use(cookieParser())
 module.exports = app
