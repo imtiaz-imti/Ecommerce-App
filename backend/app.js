697b725a7ba6c67c17c7d3d9bd41bf716f352eef
imtiaz-imti <151422815+imtiaz-imti@users.noreply.github.com>
2024-09-14 11:58:26 +0600

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -7,13 +7,13 @@ const {sendToken} = require('./extra')
 const cookieParser = require('cookie-parser')
 const app = express()
 const cors = require('cors')
-app.use(cookieParser())
 app.use(cors({
     origin: 'https://ecommerce-sb7c.onrender.com',
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'HEAD', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Origin', 'X-Requested-With', 'Accept', 'x-client-key', 'x-client-token', 'x-client-secret', 'Authorization'],
     credentials: true
 }))
+app.use(cookieParser())
 app.use(express.json())
 app.use('/api/v1',product)
 app.use('/api/v1/user',user)
