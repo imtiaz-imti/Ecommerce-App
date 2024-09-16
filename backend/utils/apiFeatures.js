7a2ed609e8a40a3478c77291897bf100d5201157
imtiaz-imti <151422815+imtiaz-imti@users.noreply.github.com>
2024-09-16 19:55:40 +0600

    Update apiFeatures.js

--- backend/utils/apiFeatures.js
+++ backend/utils/apiFeatures.js
@@ -6,8 +6,8 @@ class ApiFeatures{
    }
    async search(){
      this.query = await product.find()
-     const keyword = this.qrstr.keyword ? {name: {$regex:this.qrstr.keyword,$options:'i'}} : 'Empty'
-     return keyword 
+     const keyword = this.qrstr.keyword.keys(0) ? {name: {$regex:this.qrstr.keyword,$options:'i'}} : 'Empty'
+     return this.qrstr.keyword.keys(0) 
      if(keyword === 'Empty'){return this}
      this.query = await product.find(keyword)
      return this 
